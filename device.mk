b036373d25ff8afef8f625ae8c5fb323ed5cfb10
LuK1337 <priv.luk@gmail.com>
2020-09-27 17:45:33 +0200

    angler: Use libcutils-v29.so for libdpmframework.so
    * v30 one doesn't have `strdup8to16` symbol in it.
    
    Change-Id: Id2b4bb3017bc24c817dbe4b03834960aa1a4b444

--- device.mk
+++ device.mk
@@ -332,7 +332,8 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 29
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v29.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
 
 # VTS Tests
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
