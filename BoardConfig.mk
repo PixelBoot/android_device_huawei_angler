95de96c65a49f88389d790c8be8c1f9030f20170
PixelBoot <bootingpixels@gmail.com>
2021-02-24 13:02:25 -0800

    angler: shims: remove libprotobuf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,6 @@ TARGET_LD_SHIM_LIBS := \
     /vendor/bin/ATFWD-daemon|libcutils_shim.so \
     /vendor/lib64/libcne.so|libcutils_shim.so \
     /vendor/lib64/libril-qc-qmi-1.so|libaudioclient_shim.so \
-    /vendor/lib64/mediadrm/libwvdrmengine.so|libprotobuf_shim.so
 
 # Testing related defines
 BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/angler-setup.sh
