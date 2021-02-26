7c95f9d1b3ff9dd39a0b6981c2c395042ee92fdb
PixelBoot <bootingpixels@gmail.com>
2021-02-25 22:51:25 -0800

    angler: camera: include utils/Timers.h

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -29,7 +29,8 @@ LOCAL_CFLAGS += -D_ANDROID_
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/inc \
     $(LOCAL_PATH)/../common \
-    system/media/camera/include
+    system/media/camera/include \
+    system/core/libutils/include
 
 LOCAL_CFLAGS += -DCAMERA_ION_HEAP_ID=ION_IOMMU_HEAP_ID
 LOCAL_C_INCLUDES+= $(kernel_includes)
