00dd170405694fd70bc36f2d4541dc3d12abc4ab
PixelBoot <bootingpixels@gmail.com>
2022-03-07 12:35:01 -0800

    angler: drop AUX support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,8 +118,6 @@ BOARD_VENDORIMAGE_SQUASHFS_COMPRESSOR := lz4
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
 
-TARGET_AUX_OS_VARIANT_LIST := angler
-
 TARGET_RECOVERY_FSTAB = device/huawei/angler/rootdir/etc/recovery.fstab
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_RELEASETOOLS_EXTENSIONS := device/huawei/angler
