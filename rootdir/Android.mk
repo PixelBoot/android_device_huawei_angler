80ff93d54b32c0c7ccbb8f91c700c579dd87d944
PixelBoot <bootingpixels@gmail.com>
2022-03-07 12:46:29 -0800

    angler: rootdir: move ueventd to vendor

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -91,9 +91,9 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := ueventd.angler.rc
+LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/ueventd.angler.rc
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
-
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
