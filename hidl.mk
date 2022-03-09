cc227012842507fdef37e7b17b7ef7ac5fc0320f
PixelBoot <bootingpixels@gmail.com>
2022-03-09 12:50:29 -0800

    angler: disable configstore

--- hidl.mk
+++ hidl.mk
@@ -35,6 +35,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
+# ConfigStore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
